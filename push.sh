4579c055c33c9ffc1178ef3715849d7d6bf9fbf3
watchdog1023 <Naickercd@gmail.com>
2020-01-18 21:20:39 +0200

    Update push.sh

--- push.sh
+++ push.sh
@@ -6,14 +6,14 @@ setup_git() {
 }
 
 commit_website_files() {
-  git checkout -b master
+  git checkout -b travis
   git add opencv/opencv-3.3.1.tar.gz
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
   git remote add travis https://${GH_TOKEN}@github.com/watchdog1023/pre-built-dependencies.git > /dev/null 2>&1
-  git push --quiet --set-upstream travis master 
+  git push --quiet --set-upstream master travis
 }
 
 setup_git
